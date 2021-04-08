f311d2f84011c4339bc8f15de73bc54f45836df4
oge-nna <ogennaogbogu@gmail.com>
2021-04-08 12:23:55 +0100

    made changes to design, style and script of calculator

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ class Calculator {
             case '-':
                 computation = prev - current
                 break
-           case '*':
+           case 'x':
                 computation = prev * current
                 break
-            case '÷':
+            case '/':
                 computation = prev / current
                 break
             default:
